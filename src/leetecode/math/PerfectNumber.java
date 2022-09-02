69d15c012790925f6faf54725b95ae99dfbe0782
Prashant Ahire <pahire@roku.com>
2022-09-01 17:20:57 -0700

    Dynamo prob

--- src/leetecode/math/PerfectNumber.java
+++ src/leetecode/math/PerfectNumber.java
@@ -1,5 +1,9 @@
 package leetecode.math;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
 public class PerfectNumber {
     // Time O(n);
     public boolean checkPerfectNumber(int num) {
@@ -12,5 +16,11 @@ public class PerfectNumber {
             divsor++;
         }
         return sum == num;
+    }
+
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.of(2022, Month.DECEMBER, 31);
+        LocalDate prevDate = LocalDate.of(2022, Month.DECEMBER, 1);
+        System.out.println(prevDate.until(date, ChronoUnit.DAYS));
     }
 }
